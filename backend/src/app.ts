92ec2d72543bfa2d4ab2ff7f8400ea95a535c4cf
Deepak Kumar <deepak@Deepaks-MacBook-Air.local>
2025-04-04 11:56:25 +0530

    added folder and files for question module.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,29 +1,32 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import connectDb from './config/db';
 
 dotenv.config();
+
+connectDb();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
   res.setHeader('Content-Type', 'text/plain');
   res.end("hello")
 });
 
 // Sample Questions Data
 const questions = [
-    { id: 1, question: "What is Node.js?", answer: "Node.js is a runtime..." },
-    { id: 2, question: "What is TypeScript?", answer: "TypeScript is a superset of JavaScript..." }
-  ];
+  { id: 1, question: "What is Node.js?", answer: "Node.js is a runtime..." },
+  { id: 2, question: "What is TypeScript?", answer: "TypeScript is a superset of JavaScript..." }
+];
 
-  app.get('/api/questions', (req, res)=>{
-    res.json(questions);
-  });
+app.get('/api/questions', (req, res) => {
+  res.json(questions);
+});
 
-  const PORT = process.env.PORT || 8080
-  app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`)
-  })
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
